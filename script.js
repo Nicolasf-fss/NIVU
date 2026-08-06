12c3fea8a8a72e480e2c6f8f514070a9cca4a6a4
Nicolasf-fss <nicolasflorez32@gmail.com>
2026-08-06 02:08:13 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 window.addEventListener("load", () => {
 
     const splash = document.getElementById("splash");
+    const hero = document.getElementById("hero");
+    const btn = document.getElementById("explorar");
 
+    // Ocultar splash después de 2 segundos
     setTimeout(() => {
-
         splash.classList.add("hide");
-
     }, 2000);
 
-});
+    // Al tocar "Explorar en tiempo real"
+    btn.addEventListener("click", () => {
 
-window.addEventListener('load', () => {
+        // Desaparece el hero
+        hero.classList.add("hide");
 
-    const splash = document.getElementById('splash');
+        // Espera un poco y baja al mapa
+        setTimeout(() => {
+            document.getElementById("mapa")
+                .scrollIntoView({ behavior: "smooth" });
+        }, 500);
 
-    setTimeout(() => {
-        splash.classList.add('hide');
-    }, 2000);
-
-    const btn = document.getElementById('explorar');
-
-    btn.addEventListener('click', () => {
-        document.getElementById('mapa')
-            .scrollIntoView({ behavior: 'smooth' });
     });
 
 });
